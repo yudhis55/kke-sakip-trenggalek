fb5c635e92fb529feccd387374115bd7739d8915
yudhis55 <yudisputra55@gmail.com>
2025-12-11 22:47:04 +0700

    feat(routes): update routes to use Livewire components for authentication and dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Livewire\Auth\Login;
+use App\Livewire\Dashboard\Dashboard;
+use App\Livewire\Dashboard\LembarKerja;
+use App\Livewire\Dashboard\Mapping;
+use App\Livewire\Dashboard\Pengaturan;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::middleware(['guest'])->group(function () {
+//     Route::get('/login', Login::class)->name('login');
+// });
+
+Route::get('/', Login::class);
+Route::get('/dashboard', Dashboard::class);
+Route::get('/mapping', Mapping::class);
+Route::get('/lembar-kerja', LembarKerja::class);
+Route::get('/pengaturan', Pengaturan::class);
+
