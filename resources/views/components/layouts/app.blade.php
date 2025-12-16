edc1fd82f9389481084ee686f6812abfd53cc655
yudhis55 <yudisputra55@gmail.com>
2025-12-16 22:52:56 +0700

    feat: restrict access to Pengaturan menu for admin users only

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -218,14 +218,15 @@
                         {{-- <li class="menu-title"><i class="ri-more-fill"></i> <span data-key="t-pages">Pages</span>
                         </li> --}}
 
-                        <li class="nav-item">
-                            <a wire:current="active" class="nav-link menu-link" href="{{ route('pengaturan') }}"
-                                role="button" aria-expanded="false" aria-controls="sidebarAuth">
-                                <i class="mdi mdi-cog-outline"></i> <span
-                                    data-key="t-authentication">Pengaturan</span>
-                            </a>
-                        </li>
-
+                        @if (Auth::user()->role->jenis == 'admin')
+                            <li class="nav-item">
+                                <a wire:current="active" class="nav-link menu-link" href="{{ route('pengaturan') }}"
+                                    role="button" aria-expanded="false" aria-controls="sidebarAuth">
+                                    <i class="mdi mdi-cog-outline"></i> <span
+                                        data-key="t-authentication">Pengaturan</span>
+                                </a>
+                            </li>
+                        @endif
                     </ul>
                 </div>
                 <!-- Sidebar -->
