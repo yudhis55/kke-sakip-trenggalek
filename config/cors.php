f00bf337ab9e3d5244c195798fb58d25eca65b99
yudhis55 <yudisputra55@gmail.com>
2026-06-15 12:08:03 +0700

    fix(cors): update allowed_origins to allow all origins

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://sie-akip.trenggalekkab.go.id', 'https://app.repomedia.me'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
